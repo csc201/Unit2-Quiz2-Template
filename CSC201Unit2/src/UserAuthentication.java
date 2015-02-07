90d0da146df910934efcf8f060af74059be187b7
tkanchanawanchai6403 <tkanchanawanchai6403@an098046-ct103v.nvstu.edu>
2015-02-07 10:45:46 -0500

    initial

---
+++ CSC201Unit2/src/UserAuthentication.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class UserAuthentication {
+	private static String correctUsername = "Tanes";
+	private static String correctPassword = "1234";
+	private static String username;
+	private static String password;
+
+	public static void main(String[] args) {
+		username = JOptionPane.showInputDialog("Enter username.");
+		authentication();
+	}
+
+	public static boolean authentication() {
+		boolean status = false;
+		if (username.equalsIgnoreCase(correctUsername)) {
+			password = JOptionPane.showInputDialog("Enter password");
+			// Note: difference between showInputDialog and showMessageDialog
+			if (password.equals(correctPassword)) {
+				JOptionPane.showMessageDialog(null, "Welcome " + username);
+				status = true;
+			} else {
+				JOptionPane.showMessageDialog(null, "Invalid Password");
+			}
+		} else {
+			status = false;
+			JOptionPane.showMessageDialog(null, "Invalid Username");
+		}
+		return status;
+
+	}
+}
